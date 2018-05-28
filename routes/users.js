307c77927791cfc7a17405494fc4deb4b9a94dc1
João Melga <joaolucasfm@gmail.com>
2018-05-28 09:52:11 -0300

    Add página de usuário + Add FontAwesome
    Criei a página de usuário e adicionei o fontawesome no diretório "public/fonts"

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.send('Deu certo essa bosta');
 });
 
+router.get('/revenda', function (req, res, next){
+  res.render('user', { title: 'Revenda', extraCss: ['user'], extraJS: ['user'], layout: "layout"});
+});
+
+router.get('/results', function (req, res, next){
+  res.render('results', { title: 'Resultado', extraCss: ['results'], layout: "layout"});
+});
+
 module.exports = router;

c2a5a0c0df75a562ba27d7ab22f9037dce0a7973
João Melga <joaolucasfm@gmail.com>
2018-06-04 14:20:08 -0300

    Correção de bugs signup.hbs

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -1,26 +1,30 @@
 $(document).ready(() => {
-  $('input[name=userType]').on('change', () => {
-    const selectedUserType = $('input[name=userType]:checked').val();
+  $('#user-type input[type=radio]').on('change', () => {
+    const selectedUserType = $('#user-type input[type=radio]:checked').val();
 
     if (selectedUserType === 'Indústria') {
+      console.log('Industria');
       $('.producer-option').hide();
       $('.franchisee-option').hide();
       $('.dealer-option').hide();
       $('.industry-option').removeClass('d-none').show().focus();
     }
     else if (selectedUserType === 'Produtor') {
+      console.log('Produtor');
       $('.industry-option').hide();
       $('.franchisee-option').hide();
       $('.dealer-option').hide();
       $('.producer-option').removeClass('d-none').show();
     }
     else if (selectedUserType === 'Franqueado') {
+      console.log('Franqueado');
       $('.producer-option').hide();
       $('.industry-option').hide();
       $('.dealer-option').hide();
       $('.franchisee-option').removeClass('d-none').show();
     }
-    else {
+    else if (selectedUserType === 'Revenda')  {
+      console.log('Revenda');
       $('.producer-option').hide();
       $('.franchisee-option').hide();
       $('.industry-option').hide();

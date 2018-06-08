bcbca0eecab9424c6b1ae0066dfa72936e3d4ad1
João Melga <joaolucasfm@gmail.com>
2018-06-08 00:53:58 -0300

    Add página products/new

---
+++ public/javascripts/newproduct.js
@@ -0,0 +1,37 @@
+
+$(document).ready(() => {
+  $('#user-type input[type=radio]').on('change', () => {
+    const selectedUserType = $('#user-type input[type=radio]:checked').val();
+
+    if (selectedUserType === 'Indústria') {
+      console.log('Industria');
+      $('.producer-option').hide();
+      $('.franchisee-option').hide();
+      $('.dealer-option').hide();
+      $('.industry-option').removeClass('d-none').show().focus();
+    }
+    else if (selectedUserType === 'Produtor') {
+      console.log('Produtor');
+      $('.industry-option').hide();
+      $('.franchisee-option').hide();
+      $('.dealer-option').hide();
+      $('.producer-option').removeClass('d-none').show();
+    }
+    else if (selectedUserType === 'Franqueado') {
+      console.log('Franqueado');
+      $('.producer-option').hide();
+      $('.industry-option').hide();
+      $('.dealer-option').hide();
+      $('.franchisee-option').removeClass('d-none').show();
+    }
+    else if (selectedUserType === 'Revendedor') {
+      console.log('Revenda');
+      $('.producer-option').hide();
+      $('.franchisee-option').hide();
+      $('.industry-option').hide();
+      $('.dealer-option').removeClass('d-none').show();
+    }
+
+    $('#form-buttons').removeClass('d-none').show(); // Classe bootstrap que esconde o elemento
+  });
+});

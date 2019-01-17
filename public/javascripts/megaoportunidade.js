51f8d29333dbe38626fe9e355241e5e5c2a21180
Marina Faria <maarina.faria@gmail.com>
2019-01-17 20:26:53 -0200

    Front funcional da MegaOportunidade quando se cria uma oferta

--- public/javascripts/megaoportunidade.js
+++ public/javascripts/megaoportunidade.js
@@ -1,20 +1,44 @@
 $(document).ready(() => {
-  $('#select').on('change', () => {
-     $('#megaOpportunity').empty();
-      const badgeHtml = `
-      <div class="user-input">
-        <br/>
-        <div class="input-group mb-2">
-         <div class="input-group-prepend">
-           <div class="input-group-text"><i class="fas fa-car"></i></div>
-         </div>
-         <select class="form-control new-shadow" name="offer[delivery]" required/>
-           <option disabled selected value>-- Forma de entrega --</option>
-           <option value="31 dias">Em até 31 dias</option>
-           <option value="48 horas">Em até 48 horas</option>
-         </select>
-        </div>
-      </div>`;
-      $('#megaOpportunity').append(badgeHtml);
+  $(() => {
+      $('#mega-price').hide();
+      $('#mega-block-delivery').hide();
+
+      $('#mega').on('click', function () {
+          if ($(this).prop('checked')) {
+              $('#mega-price').fadeIn();
+              $('#mega-price-input').attr('required','required');
+              $('#low-price').hide();
+              $('#low-price-input').removeAttr('required');
+              $('#average-price').hide();
+              $('#average-price-input').removeAttr('required');
+              $('#high-price').hide();
+              $('#high-price-input').removeAttr('required');
+              $('#mega-block-delivery').fadeIn();
+              $('#change-prices').hide();
+              $('#low-breakpoint').removeAttr('required');
+              $('#average-breakpoint').removeAttr('required');
+              $('#normal-delivery').removeAttr('required');
+              $('#mega-delivery').attr('required','required');
+              $('#low-breakpoint').val(2)
+              $('#average-breakpoint').val(1)
+          } else {
+              $('#mega-price').hide();
+              $('#mega-price-input').removeAttr('required');
+              $('#low-price').fadeIn();
+              $('#low-price-input').attr('required','required');
+              $('#average-price').fadeIn();
+              $('#average-price-input').attr('required','required');
+              $('#high-price').fadeIn();
+              $('#high-price-input').attr('required','required');
+              $('#mega-block-delivery').hide();
+              $('#change-prices').fadeIn();
+              $('#low-breakpoint').attr('required','required');
+              $('#average-breakpoint').attr('required','required');
+              $('#normal-delivery').attr('required','required');
+              $('#mega-delivery').removeAttr('required');
+              $('#low-breakpoint').val(0)
+              $('#average-breakpoint').val(0)
+          }
+      });
   });
 });

27f990126dccbf03761d1008a06ce5afb7c07f27
Felipe Souza <felipesouza@cpejr.om.br>
2019-02-01 17:32:00 -0200

    Correção ferramenta de busca e ícone mega Oportunidade

--- routes/search.js
+++ routes/search.js
@@ -19,6 +19,7 @@ router.get('/', (req, res) => {
     console.log(products.length);
     if (products.length !== 0) {
       products.forEach((product) => {
+        console.log(product);
         const queryOffer = { product: product._id, $or: [{ delivery: '48 horas' }, { megaOpportunity: true }], active: true };
         const sortOffer = { 'product.name': 1, delivery: -1, 'price.low': 1 };
         const queryGroup = { productId: product._id, active: true };
@@ -30,8 +31,16 @@ router.get('/', (req, res) => {
       });
       Promise.all(offerPromises).then((offerResults) => {
         Promise.all(groupPromises).then((groupResults) => {
-          const groups = groupResults[0].concat(groupResults[1]);
-          const offers = offerResults[0].concat(offerResults[1]);
+          console.log(offerResults);
+          console.log(groupResults);
+          let groups = groupResults[0].concat(groupResults[1]);
+          let offers = offerResults[0].concat(offerResults[1]);
+          for (let i = 2; i < groupResults.length; i++) {
+            groups = groups.concat(groupResults[i]);
+          }
+          for (let i = 2; i < offerResults.length; i++) {
+            offers = offers.concat(offerResults[i]);
+          }
           console.log(groups);
           console.log(offers);
 
